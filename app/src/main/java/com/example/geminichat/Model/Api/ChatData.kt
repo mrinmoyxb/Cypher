58c0fc4a4f0aa636041e92e86a0efc0468d937a1
Mrinmoy Borah <141025752+mrinmoyxb@users.noreply.github.com>
2024-01-29 22:05:16 +0530

    Done with Gemini Chat Data

--- app/src/main/java/com/example/geminichat/Model/Api/ChatData.kt
+++ app/src/main/java/com/example/geminichat/Model/Api/ChatData.kt
@@ -1,8 +1,69 @@
 package com.example.geminichat.Model.Api
 
+import android.graphics.Bitmap
+import com.example.geminichat.Model.Data.ChatClass
 import com.google.ai.client.generativeai.BuildConfig
+import com.google.ai.client.generativeai.Chat
+import com.google.ai.client.generativeai.GenerativeModel
+import com.google.ai.client.generativeai.type.ResponseStoppedException
+import com.google.ai.client.generativeai.type.content
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 object ChatData {
-    //val api = BuildConfig.API_KEY
     val key = com.example.geminichat.BuildConfig.API_KEY
+
+    //prompt from user -> text prompt
+    suspend fun getResponse(prompt: String): ChatClass {
+        val generativeModel = GenerativeModel(
+            modelName = "gemini-pro", apiKey = key
+        )
+
+        try{
+            val response = withContext(Dispatchers.IO){
+                generativeModel.generateContent(prompt)
+            }
+
+            return ChatClass(
+                prompt = response.text?:"error",
+                bitmap = null,
+                isFromUser = false
+            )
+        } catch(e: ResponseStoppedException){
+            return ChatClass(
+                prompt = e.message?:"error",
+                bitmap = null,
+                isFromUser = false
+            )
+        }
+    }
+
+    //prompt from user -> image prompt
+    suspend fun getResponse(prompt: String, bitmap: Bitmap): ChatClass {
+        val generativeModel = GenerativeModel(
+            modelName = "gemini-pro-vision", apiKey = key
+        )
+
+        try{
+            val inputContent = content{
+                image(bitmap)
+                text(prompt)
+            }
+            val response = withContext(Dispatchers.IO){
+                generativeModel.generateContent(prompt)
+            }
+
+            return ChatClass(
+                prompt = response.text?:"error",
+                bitmap = null,
+                isFromUser = false
+            )
+        } catch(e: ResponseStoppedException){
+            return ChatClass(
+                prompt = e.message?:"error",
+                bitmap = null,
+                isFromUser = false
+            )
+        }
+    }
 }
--- app/src/main/java/com/example/geminichat/Model/Data/Chat.kt
+++ app/src/main/java/com/example/geminichat/Model/Data/Chat.kt
@@ -2,7 +2,7 @@ package com.example.geminichat.Model.Data
 
 import android.graphics.Bitmap
 
-data class Chat(
+data class ChatClass(
     val prompt: String,
     val bitmap: Bitmap?,
     val isFromUser: Boolean
